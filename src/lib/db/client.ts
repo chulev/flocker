6afeea384aec0bac61ebcffbda9cb6d157c30eb4
Georgi Chulev <gichulev@gmail.com>
2024-04-08 09:59:16 +0300

    build: use neon serverless postgres on production

--- src/lib/db/client.ts
+++ src/lib/db/client.ts
@@ -1,13 +1,19 @@
 import { Kysely, PostgresDialect } from 'kysely'
+import { NeonDialect } from 'kysely-neon'
 import { Pool } from 'pg'
 
 import { DB } from './schema'
 
+const dialect =
+  process.env.NODE_ENV === 'production'
+    ? new NeonDialect({ connectionString: process.env.DATABASE_URL })
+    : new PostgresDialect({
+        pool: new Pool({
+          connectionString: process.env.DATABASE_URL,
+          max: 10,
+        }),
+      })
+
 export const db = new Kysely<DB>({
-  dialect: new PostgresDialect({
-    pool: new Pool({
-      connectionString: process.env.DATABASE_URL,
-      max: 10,
-    }),
-  }),
+  dialect,
 })
