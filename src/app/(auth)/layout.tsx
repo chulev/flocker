85af4cec9785031bae20f77933ef5252631f164b
Georgi Chulev <gichulev@gmail.com>
2025-01-16 12:58:26 +0200

    fix: update auth layout min height

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -6,7 +6,7 @@ export default function AuthLayout({
   children: React.ReactNode
 }) {
   return (
-    <section className='grid bg-auth'>
+    <section className='grid min-h-screen bg-auth'>
       <section className='mx-auto flex w-[400px] flex-col justify-center border border-mist bg-pure p-8 max-sm:w-full'>
         <Logo />
         <div className='mt-6'>{children}</div>
